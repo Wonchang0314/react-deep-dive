e125902cf74ebd338f76a18cfc5b94b78d8bc6ab
Wonchang0314 <andrew0314@naver.com>
2025-01-30 17:52:38 +0900

    createElement 함수 구현

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,19 @@
 import "./style.css";
+import { createElement, JSXPropT } from "./create";
+
+const children = {
+  key: "2",
+  content: "Hello, React Clone!",
+  type: "h1",
+};
+
+const element: JSXPropT = {
+  key: "1",
+  props: [children],
+  type: "div",
+};
+
+const testElement = createElement(element);
+console.log(testElement.outerHTML);
 
 document.querySelector<HTMLDivElement>("#app")!;
